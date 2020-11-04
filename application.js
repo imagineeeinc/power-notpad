a1e0c4e43b6f0fa38e698b364cabcf24b654c264
Imagineeeinc <imagineeelego@gmail.com>
2020-11-04 20:42:34 +0000

    added to application.js
    Signed-off-by: Imagineeeinc <imagineeelego@gmail.com>

--- application.js
+++ application.js
@@ -2,6 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
+    //frame: false,
+    //backgroundColor: '#3b10e6',
+    fullscreenable: true,
+    hasShadow: true,
     width: 800,
     height: 600,
     webPreferences: {
